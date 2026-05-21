1a828caf583dab4974226ccfc358158d859107ff
switchprog <99533936+switchprog@users.noreply.github.com>
2026-05-22 01:39:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 #include "dd_type.h"
 #include "dd_trig.h"
 
 int main(int argc, char *argv[]) {
     DoubleDouble x;
     
-    // Чтение старшей части
+    if (argc < 2) {
+        return 1;
+    }
+    
     x.hi = atof(argv[1]);
     
-    // Чтение младшей части
     if (argc > 2) {
         x.lo = atof(argv[2]);
     } else {
         x.lo = 0.0;
     }
     
-    // Вычисление тангенса
     DoubleDouble result = dd_tan(x);
     
-    // Вывод результата
     printf("%.17e %.17e\n", result.hi, result.lo);
     
     return 0;
