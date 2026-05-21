81e8d97f6759e1245ee369b2fcc13a271c86c0c6
switchprog <99533936+switchprog@users.noreply.github.com>
2026-05-22 01:36:32 +0300

    Update dd_div.c

--- dd_div.c
+++ dd_div.c
@@ -2,13 +2,11 @@
 #include "dd_mul.h"
 #include "dd_arith.h"
 
-// Деление через итерации Ньютона для обратного
 DoubleDouble dd_div(DoubleDouble a, DoubleDouble b) {
     double inv_hi = 1.0 / b.hi;
     DoubleDouble inv = {inv_hi, 0.0};
     DoubleDouble two = {2.0, 0.0};
     
-    // Три итерации
     for (int i = 0; i < 3; i++) {
         DoubleDouble b_inv = dd_mul(b, inv);
         DoubleDouble two_minus = dd_sub(two, b_inv);
