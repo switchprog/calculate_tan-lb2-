24aa235ee5ad4367858ffd40afa97828a4a2c6bf
switchprog <99533936+switchprog@users.noreply.github.com>
2026-05-22 01:39:01 +0300

    Update dd_type.h

--- dd_type.h
+++ dd_type.h
@@ -1,19 +1,17 @@
 #ifndef DD_TYPE_H
 #define DD_TYPE_H
 
-// Структура для числа двойной точности
 typedef struct {
-    double hi;  // старшая часть
-    double lo;  // младшая часть
+    double hi;
+    double lo;
 } DoubleDouble;
 
-// Внешние константы
 extern const DoubleDouble DD_PI;
 extern const DoubleDouble DD_PI_2;
 extern const DoubleDouble DD_PI_4;
 extern const DoubleDouble DD_2PI;
-extern const DoubleDouble FACTORIAL_EVEN[];
-extern const DoubleDouble FACTORIAL_ODD[];
+extern const DoubleDouble FACTORIAL_EVEN[201];
+extern const DoubleDouble FACTORIAL_ODD[200];
 extern const double INV_PI[];
 extern const double P_COEFF[];
 
